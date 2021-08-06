1863c9eac8636dab8fb435b3c265259615c9e8d1
Noah <themathmaster3000@gmail.com>
2021-08-05 21:31:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@ from setuptools import find_packages, setup
 with open('squares/_version.py') as version_file:
     exec(version_file.read())
 
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
 setup(
     name='squares-rng',
     version=__version__,
     packages=['squares'],
     description='A simple counter-based pseudo random number generator implementation based on arXiv:2004.06278',
+    long_description=README,
+    url='https://github.com/Oafish1/Squares',
+    author='Oafish1',
     test_suite='tests',
     install_requires=[]
 )
